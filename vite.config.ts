8a614d0d9e1abdfcb34b941d9f74789f652b00fa
Yan Harazhun <harazhun06@gmail.com>
2025-04-18 17:09:15 +0200

    Update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
   base: '/payment-app/',
-  build: {
-    outDir: 'dist',
-  },
+  plugins: [react()],
 })
